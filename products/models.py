687571df79deee7a40f24086db46d4c4c2d3b1fd
SantiBert <santibertero34@gmail.com>
2022-05-01 03:08:50 -0300

    primer commit

---
+++ products/models.py
@@ -0,0 +1,91 @@
+from django.utils import timezone
+from django.db import models
+
+from autoslug import AutoSlugField
+
+
+CHOICES = (
+	("1", "Disponible"),
+	("2", "No disponible")
+)
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+    slug = AutoSlugField(populate_from='name')
+    status = models.CharField(max_length=10, choices=CHOICES)
+    created_date = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+    slug = AutoSlugField(populate_from='name')
+    status = models.CharField(max_length=10, choices=CHOICES)
+    created_date = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Subcategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, unique=True)
+    slug = AutoSlugField(populate_from='name')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    status = models.CharField(max_length=10, choices=CHOICES)
+    created_date = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    brand = models.ForeignKey(Brand, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    subcategory = models.ForeignKey(Subcategory, on_delete=models.PROTECT)
+    name = models.CharField(max_length=255)
+    slug = AutoSlugField(populate_from='name')
+    price = models.FloatField(max_length=100)
+    code = models.CharField(max_length=10, null=True, blank=True)
+    #image = models.ImageField(upload_to="media/product/images/")
+    quantity = models.IntegerField(default=0)
+    description = models.TextField(null=True, blank=True)
+    taxs = models.FloatField(max_length=100, null=True, blank=True)
+    status = models.CharField(max_length=10, choices=CHOICES)
+    created_date = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    date = models.DateTimeField(auto_now_add=True)
+    sub_total = models.FloatField(max_length=100)
+    vat = models.FloatField(max_length=100)
+    total_amount = models.FloatField(max_length=100)
+    discount = models.FloatField(max_length=100)
+    grand_total = models.FloatField(max_length=100)
+    paid = models.FloatField(max_length=100)
+    due = models.FloatField(max_length=100)
+    payment_type = models.CharField(max_length=100)
+    payment_status = models.IntegerField()
+    status = models.IntegerField()
+    created_date = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
+
+class OrderItem(models.Model):
+    id = models.AutoField(primary_key=True)
+    order_id = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product_id = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.IntegerField()
+    rate = models.FloatField(max_length=100)
+    total = models.FloatField(max_length=100)
+    status = models.IntegerField()
+    created_date = models.DateTimeField(default=timezone.now)
+    is_active = models.BooleanField(default=True)
